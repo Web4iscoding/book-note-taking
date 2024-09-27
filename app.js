30a005e7cc0f0da347585abf11b7ea9594d6b1fe
Web <mettatonthepowerofex@gmail.com>
2024-09-28 00:23:52 +0800

    Finished project

---
+++ app.js
@@ -0,0 +1,89 @@
+const myLibrary = [];
+
+const submit = document.querySelector("button[type='button']");
+const space = document.querySelector(".partition2 > div")
+const display = document.querySelector(".display");
+const dialog = document.querySelector("dialog");
+const add = document.querySelector("button:first-of-type");
+const form = document.querySelector("form");
+const dialogClose = document.querySelector("dialog>div");
+
+// Constructor 
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const author = document.querySelector("#author").value;
+    const title = document.querySelector("#title").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#yes").checked ? "Yes" : "No";
+    console.log(read)
+    const book = new Book(author, title, pages, read);
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
+
+function displayBooks() {
+    for (let item of myLibrary) {
+        const card = document.createElement("div");
+        const close = document.createElement("div");
+        const change = document.createElement("div");
+        close.classList.add("card-close");
+        change.classList.add("change")
+        const author = document.createElement("div");
+        const title = document.createElement("div");
+        const pages = document.createElement("div");
+        const read = document.createElement("div");
+        author.textContent = "Author: " + item.author;
+        title.textContent = "Title: " + item.title;
+        pages.textContent = "Pages: " + item.pages;
+        read.textContent = "Read: " + item.read;
+        card.appendChild(close);
+        card.appendChild(change);
+        card.appendChild(author);
+        card.appendChild(title);
+        card.appendChild(pages);
+        card.appendChild(read);
+        space.appendChild(card);
+        card.addEventListener("click", (e) => {
+            if (e.target.className === "card-close") {
+                space.removeChild(e.target.parentNode);
+                myLibrary.splice(myLibrary.indexOf(item), 1);
+            }
+        })
+        change.addEventListener("click", (e) => {
+            item.read = item.read === "Yes" ? "No" : "Yes";
+            read.textContent = "Read: " + item.read;
+        })
+    }
+}
+
+add.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+submit.addEventListener("click", (e) => {
+    const author = document.querySelector("#author").value;
+    const title = document.querySelector("#title").value;
+    const pages = document.querySelector("#pages").value;
+    if (!(author.trim().length === 0 || title.trim().length === 0 || pages.trim().length === 0)) {
+        addBookToLibrary()
+        form.reset();
+        dialog.close();
+        space.innerHTML = "";
+        displayBooks();
+    }
+});
+
+// addEventListener("DOMContentLoaded", () => {
+//     dialog.showModal();
+// })
+
+dialogClose.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+})
